a1962d9bf869a05916a7b31fb1c0af4929ec5a36
Martin Wickham <spexguy070@gmail.com>
2023-12-01 23:16:51 -0600

    Day 2

--- src/day02.zig
+++ src/day02.zig
@@ -11,7 +11,32 @@ const gpa = util.gpa;
 const data = @embedFile("data/day02.txt");
 
 pub fn main() !void {
-    
+    var p1: i64 = 0;
+    var p2: i64 = 0;
+    var lines = tokenizeAny(u8, data, "\r\n");
+    while (lines.next()) |line| {
+        var red: i64 = 0;
+        var green: i64 = 0;
+        var blue: i64 = 0;
+        var toks = tokenizeAny(u8, line[5..], ": ,;");
+        const id = try parseInt(i64, toks.next().?, 10);
+        while (toks.next()) |num_s| {
+            const num = try parseInt(i64, num_s, 10);
+            const color = toks.next().?;
+            if (std.mem.eql(u8, color, "red")) {
+                red = @max(red, num);
+            } else if (std.mem.eql(u8, color, "green")) {
+                green = @max(green, num);
+            } else if (std.mem.eql(u8, color, "blue")) {
+                blue = @max(blue, num);
+            }
+        }
+        if (red <= 12 and green <= 13 and blue <= 14) {
+            p1 += id;
+        }
+        p2 += red * blue * green;
+    }
+    std.debug.print("part1: {}, part2: {}\n", .{p1, p2});
 }
 
 // Useful stdlib functions
