3a6943d0699a95855baf2c69396fb9876dc55600
Martin Wickham <spexguy070@gmail.com>
2023-12-05 00:31:49 -0600

    Day 04

--- src/day04.zig
+++ src/day04.zig
@@ -11,7 +11,47 @@ const gpa = util.gpa;
 const data = @embedFile("data/day04.txt");
 
 pub fn main() !void {
-    
+    var p1: usize = 0;
+    var p2: usize = 0;
+    var lines = tokenizeSca(u8, data, '\n');
+    var cards = List(usize).init(gpa);
+    while (lines.next()) |line| {
+        var numbers = tokenizeAny(u8, line[4..], ": ");
+        const id = parseInt(usize, numbers.next().?, 10) catch unreachable;
+        _ = id;
+        var items = std.BoundedArray(usize, 32){};
+        while (numbers.next()) |nstr| {
+            if (nstr[0] == '|') break;
+            items.append(parseInt(usize, nstr, 10) catch unreachable) catch unreachable;
+        }
+        var matches: usize = 0;
+        while (numbers.next()) |yourstr| {
+            const yournum = parseInt(usize, yourstr, 10) catch unreachable;
+            if (indexOf(usize, items.slice(), yournum)) |idx| {
+                _ = items.swapRemove(idx);
+                matches += 1;
+            }
+        }
+        cards.append(matches) catch unreachable;
+        if (matches > 0) {
+            p1 += @as(usize, 1) << @intCast(matches-1);
+        }
+    }
+
+    const copies = gpa.alloc(usize, cards.items.len) catch unreachable;
+    @memset(copies, 1);
+    for (cards.items, 0..) |score, i| {
+        for (i+1..score+i+1) |j| {
+            if (j >= copies.len) break;
+            copies[j] += copies[i];
+        }
+    }
+
+    for (copies) |num| {
+        p2 += num;
+    }
+
+    std.debug.print("p1: {}, p2: {}\n", .{p1, p2});
 }
 
 // Useful stdlib functions
