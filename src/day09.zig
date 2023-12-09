17397c8c4e824989edd1ffcd0279664f750ae5d8
Martin Wickham <spexguy070@gmail.com>
2023-12-08 23:26:31 -0600

    Day 09

--- src/day09.zig
+++ src/day09.zig
@@ -13,16 +13,44 @@ const data = @embedFile("data/day09.txt");
 pub fn main() !void {
     var p1: i64 = 0; _ = &p1;
     var p2: i64 = 0; _ = &p2;
-    var lines = splitSca(u8, data, '\n');
+    var lines = tokenizeSca(u8, data, '\n');
     while (lines.next()) |line| {
-
+        var parts = tokenizeSca(u8, line, ' ');
+        var sequence = List(i64).init(gpa);
+        while (parts.next()) |part| {
+            const num = parseDec(part);
+            sequence.append(num) catch unreachable;
+        }
+        const prevnext = extrapolate(sequence.items);
+        p1 += prevnext[1];
+        p2 += prevnext[0];
     }
 
     print("p1: {}, p2: {}\n", .{p1, p2});
 }
 
+fn extrapolate(sequence: []const i64) [2]i64 {
+    assert(sequence.len > 0);
+    const deltas = gpa.alloc(i64, sequence.len - 1) catch unreachable;
+    defer gpa.free(deltas);
+    var has_nonzero = false;
+    for (0..sequence.len-1) |i| {
+        deltas[i] = sequence[i+1] - sequence[i];
+        if (deltas[i] != 0) has_nonzero = true;
+    }
+    if (!has_nonzero) return .{sequence[0], sequence[sequence.len-1]};
+    const prevnext = extrapolate(deltas);
+    return .{
+        sequence[0] - prevnext[0],
+        sequence[sequence.len-1] + prevnext[1],
+    };
+}
+
 fn parseDec(val: []const u8) i64 {
-    return parseInt(i64, val, 10) catch unreachable;
+    return parseInt(i64, val, 10) catch |err| {
+        print("Not a decimal number: '{s}' ({})\n", .{val, err});
+        unreachable;
+    };
 }
 
 // Useful stdlib functions
