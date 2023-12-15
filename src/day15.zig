3254d65e984a059d68df768ca79cdb8cc8a3a807
Martin Wickham <spexguy070@gmail.com>
2023-12-14 23:23:10 -0600

    Day 15

--- src/day15.zig
+++ src/day15.zig
@@ -10,19 +10,45 @@ const gpa = util.gpa;
 
 const data = @embedFile("data/day15.txt");
 
-pub fn main() !void {
-    var p1: i64 = 0; _ = &p1;
-    var p2: i64 = 0; _ = &p2;
-    var lines = splitSca(u8, data, '\n');
-    while (lines.next()) |line| {
-
+fn hash(str: []const u8) usize {
+    var h: usize = 0;
+    for (str) |c| {
+        h = ((h + c) * 17) & 0xFF;
     }
-
-    print("p1: {}, p2: {}\n", .{p1, p2});
+    return h;
 }
 
-fn parseDec(val: []const u8) i64 {
-    return parseInt(i64, val, 10) catch unreachable;
+pub fn main() !void {
+    var p1: usize = 0;
+    var p2: usize = 0;
+    var lines = tokenizeAny(u8, data, ",\n");
+    var lenses = std.StringArrayHashMap(usize).init(gpa);
+    while (lines.next()) |line| {
+        p1 += hash(line);
+        if (line[line.len - 1] == '-') {
+            const label = line[0 .. line.len - 1];
+            _ = lenses.orderedRemove(label);
+        } else {
+            const sp = indexOf(u8, line, '=').?;
+            const name = line[0..sp];
+            const amt = parseDec(line[sp + 1 ..]);
+            try lenses.put(name, amt);
+        }
+    }
+
+    var positions = [_]usize{0} ** 256;
+    for (lenses.keys(), lenses.values()) |name, foc| {
+        const box = hash(name);
+        const pos = positions[box];
+        positions[box] += 1;
+        p2 += (box + 1) * (pos + 1) * foc;
+    }
+
+    print("p1: {}, p2: {}\n", .{ p1, p2 });
+}
+
+fn parseDec(val: []const u8) usize {
+    return parseInt(usize, val, 10) catch unreachable;
 }
 
 // Useful stdlib functions
