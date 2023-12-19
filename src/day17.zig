2ea7c5ea160f937ff3a686b1869e00ae809b2daa
Martin Wickham <spexguy070@gmail.com>
2023-12-19 00:49:58 -0600

    Move pathfinding and grid code into utils

--- src/day17.zig
+++ src/day17.zig
@@ -7,113 +7,58 @@ const BitSet = std.DynamicBitSet;
 
 const util = @import("util.zig");
 const gpa = util.gpa;
+const Grid = util.Grid;
+const Dir = util.Dir;
+const NeighborCollector = util.NeighborCollector;
 
 const data = @embedFile("data/day17.txt");
-
-const right = 0;
-const up = 1;
-const left = 2;
-const down = 3;
-
-fn step(pos: usize, dir: u2, pitch: usize) usize {
-    return switch (dir) {
-        right => pos + 1,
-        up => pos - pitch,
-        left => pos - 1,
-        down => pos + pitch,
-    };
-}
 
 const State = struct {
     pos: usize,
     dir: u2,
     moves: u8,
 };
-const StateInfo = struct {
-    cost: usize,
-    prev: usize,
-};
 
-const StateCost = struct {
-    state: State,
-    cost: usize,
-};
+const Context = struct {
+    g: Grid,
+    min_moves: u8,
+    max_moves: u8,
 
-const IdCost = struct {
-    id: usize,
-    cost: usize,
-};
-
-const StateCosts = std.AutoArrayHashMap(State, StateInfo);
-fn orderStates(_: void, a: IdCost, b: IdCost) std.math.Order {
-    return std.math.order(a.cost, b.cost);
-}
-
-fn findBestPath(grid: []const u8, pitch: usize, min_moves: u8, max_moves: u8) !usize {
-    const target_pos = grid.len - pitch - 3;
-    var open = std.PriorityQueue(IdCost, void, orderStates).init(gpa, {});
-    var states = StateCosts.init(gpa);
-    try states.put(.{ .pos = pitch + 1, .dir = right, .moves = max_moves }, .{ .cost = 0, .prev = 0 });
-    try open.add(.{ .id = 0, .cost = 0 });
-    const best_state = while (true) {
-        const state_id = open.remove();
-        const state = states.keys()[state_id.id];
-        const cost = states.values()[state_id.id].cost;
-        if (cost != state_id.cost) continue; // dummy entry
-        if (state.pos == target_pos) {
-            break state_id.id;
-        }
-
-        var succs = std.BoundedArray(StateCost, 3){};
-        if (walkCrucible(grid, pitch, state.pos, state.dir +% 1, min_moves, cost, max_moves - min_moves)) |succ| succs.append(succ) catch unreachable;
-        if (walkCrucible(grid, pitch, state.pos, state.dir +% 3, min_moves, cost, max_moves - min_moves)) |succ| succs.append(succ) catch unreachable;
-        if (state.moves != 0) {
-            if (walkCrucible(grid, pitch, state.pos, state.dir, 1, cost, state.moves - 1)) |succ| succs.append(succ) catch unreachable;
-        }
-
-        for (succs.slice()) |ss| {
-            const gop = try states.getOrPut(ss.state);
-            if (!gop.found_existing or ss.cost < gop.value_ptr.cost) {
-                gop.value_ptr.* = .{ .cost = ss.cost, .prev = state_id.id };
-                try open.add(.{ .id = gop.index, .cost = ss.cost });
-            }
-        }
-    };
-
-    return states.values()[best_state].cost;
-}
-
-fn walkCrucible(grid: []const u8, pitch: usize, start_pos: usize, dir: u2, num_steps: usize, base_cost: usize, new_moves: u8) ?StateCost {
-    var pos = start_pos;
-    var cost = base_cost;
-    for (0..num_steps) |_| {
-        pos = step(pos, dir, pitch);
-        if (grid[pos] == '#') return null;
-        cost += (grid[pos] - '0');
+    pub fn isTerminal(c: *@This(), s: State) bool {
+        return s.pos == c.g.botRight();
     }
-    return .{ .state = .{
-        .pos = pos,
-        .dir = dir,
-        .moves = new_moves,
-    }, .cost = cost };
+    pub fn expand(c: *@This(), nc: NeighborCollector(State)) void {
+        c.walkCrucible(nc, Dir.cw(nc.state.dir), c.min_moves, c.max_moves - c.min_moves);
+        c.walkCrucible(nc, Dir.ccw(nc.state.dir), c.min_moves, c.max_moves - c.min_moves);
+        if (nc.state.moves != 0) {
+            c.walkCrucible(nc, nc.state.dir, 1, nc.state.moves - 1);
+        }
+    }
+
+    fn walkCrucible(c: *@This(), nc: NeighborCollector(State), dir: u2, num_steps: u8, new_moves: u8) void {
+        var pos = nc.state.pos;
+        var cost: usize = 0;
+        for (0..num_steps) |_| {
+            pos = c.g.step(pos, dir);
+            if (c.g.cells[pos] == '#') return;
+            cost += (c.g.cells[pos] - '0');
+        }
+        nc.add(.{ .pos = pos, .dir = dir, .moves = new_moves }, cost);
+    }
+};
+
+fn findBestPath(g: Grid, min_moves: u8, max_moves: u8) usize {
+    var ctx: Context = .{ .g = g, .min_moves = min_moves, .max_moves = max_moves };
+    const start_state: State = .{ .pos = g.topLeft(), .dir = Dir.right, .moves = max_moves };
+    const result = util.search(State, Context, &ctx, start_state);
+    return result.found_state.?.cost;
 }
 
 pub fn main() !void {
-    const width = indexOf(u8, data, '\n').?;
-    const data_pitch = width + 1;
-    const height = @divFloor(data.len + 1, data_pitch);
-    const pitch = width + 3;
-    const grid = try gpa.alloc(u8, pitch * (height + 2));
-    @memset(grid, '#');
-    for (0..height) |y| {
-        @memcpy(grid[(y + 1) * pitch ..][1..][0..width], data[y * data_pitch ..][0..width]);
-        grid[(y + 1) * pitch ..][width + 2] = '\n';
-    }
-    grid[width + 2] = '\n';
-    grid[grid.len - 1] = '\n';
+    const g = try Grid.load(data, 1, '#');
 
-    const p1 = try findBestPath(grid, pitch, 1, 3);
-    const p2 = try findBestPath(grid, pitch, 4, 10);
+    const p1 = findBestPath(g, 1, 3);
+    const p2 = findBestPath(g, 4, 10);
 
     print("p1: {}, p2: {}\n", .{ p1, p2 });
 }
--- src/util.zig
+++ src/util.zig
@@ -10,6 +10,218 @@ var gpa_impl = std.heap.GeneralPurposeAllocator(.{}){};
 pub const gpa = gpa_impl.allocator();
 
 // Add utility functions here
+pub const Grid = struct {
+    cells: []u8,
+    pitch: usize,
+    width: usize,
+    height: usize,
+    padding: usize,
+
+    pub fn load(data: []const u8, padding: usize, pad_char: u8) !Grid {
+        const width = indexOf(u8, data, '\n').?;
+        const data_pitch = width + 1;
+        const height = @divFloor(data.len + 1, data_pitch);
+        const pitch = width + 2 * padding + 1;
+        const cells = try gpa.alloc(u8, pitch * (height + 2 * padding));
+        @memset(cells, pad_char);
+        for (0..height) |y| {
+            @memcpy(cells[(y + padding) * pitch ..][padding..][0..width], data[y * data_pitch ..][0..width]);
+            cells[(y + padding) * pitch ..][padding + width + padding] = '\n';
+        }
+        for (0..padding) |i| {
+            cells[i * pitch + padding + width + padding] = '\n';
+            cells[cells.len - i * pitch - 1] = '\n';
+        }
+
+        return .{
+            .cells = cells,
+            .pitch = pitch,
+            .width = width,
+            .height = height,
+            .padding = padding,
+        };
+    }
+
+    pub fn topLeft(g: *const Grid) usize {
+        return g.padding * g.pitch + g.padding;
+    }
+    pub fn topRight(g: *const Grid) usize {
+        return g.topLeft() + g.width - 1;
+    }
+    pub fn botLeft(g: *const Grid) usize {
+        return g.pitch * (g.padding + g.height - 1) + g.padding;
+    }
+    pub fn botRight(g: *const Grid) usize {
+        return g.botLeft() + g.width - 1;
+    }
+
+    pub fn step(g: *const Grid, pos: usize, dir: u2) usize {
+        return g.walk(pos, dir, 1);
+    }
+    pub fn walk(g: *const Grid, pos: usize, dir: u2, steps: usize) usize {
+        return switch (dir) {
+            Dir.right => pos + steps,
+            Dir.up => pos - g.pitch * steps,
+            Dir.left => pos - steps,
+            Dir.down => pos + g.pitch * steps,
+        };
+    }
+};
+
+pub const Dir = opaque {
+    pub const right: u2 = 0;
+    pub const up: u2 = 1;
+    pub const left: u2 = 2;
+    pub const down: u2 = 3;
+
+    pub const east: u2 = right;
+    pub const north: u2 = up;
+    pub const west: u2 = left;
+    pub const south: u2 = down;
+
+    pub fn flip(dir: u2) u2 {
+        return dir +% 2;
+    }
+    pub fn cw(dir: u2) u2 {
+        return dir +% 3;
+    }
+    pub fn ccw(dir: u2) u2 {
+        return dir +% 1;
+    }
+};
+
+test "Grid" {
+    var g = try Grid.load(
+        \\v.<
+        \\...
+        \\>.^
+    , 2, '#');
+    try std.testing.expectEqualStrings(
+        \\#######
+        \\#######
+        \\##v.<##
+        \\##...##
+        \\##>.^##
+        \\#######
+        \\#######
+        \\
+    , g.cells);
+    try std.testing.expectEqual(@as(usize, 3), g.width);
+    try std.testing.expectEqual(@as(usize, 3), g.height);
+    try std.testing.expectEqual(@as(u8, 'v'), g.cells[g.topLeft()]);
+    try std.testing.expectEqual(@as(u8, '<'), g.cells[g.topRight()]);
+    try std.testing.expectEqual(@as(u8, '>'), g.cells[g.botLeft()]);
+    try std.testing.expectEqual(@as(u8, '^'), g.cells[g.botRight()]);
+}
+
+pub const StateInfo = struct {
+    cost: usize,
+    prev: usize,
+};
+
+pub const StateCost = struct {
+    id: usize,
+    cost: usize,
+
+    pub fn order(_: void, a: StateCost, b: StateCost) std.math.Order {
+        return std.math.order(a.cost, b.cost);
+    }
+};
+
+pub const OpenQueue = std.PriorityQueue(StateCost, void, StateCost.order);
+
+pub fn SearchResult(comptime State: type) type {
+    return struct {
+        states: std.AutoArrayHashMap(State, StateInfo),
+        found_state: ?StateCost,
+
+        pub fn shortestPathTo(self: @This(), end: usize) []usize {
+            var arr = List(usize).init(gpa);
+            arr.append(end) catch unreachable;
+            var state = end;
+            while (state != 0) {
+                state = self.states.values()[state].prev;
+                arr.append(state) catch unreachable;
+            }
+            std.mem.reverse(usize, arr.items);
+            return arr.toOwnedSlice() catch unreachable;
+        }
+    };
+}
+
+pub fn NeighborCollector(comptime State: type) type {
+    return struct {
+        state: State,
+        id: usize,
+        cost: usize,
+        states: *std.AutoArrayHashMap(State, StateInfo),
+        open: *OpenQueue,
+
+        pub fn add(nc: *const @This(), successor: State, addl_cost: usize) void {
+            const cost = nc.cost + addl_cost;
+            const gop = nc.states.getOrPut(successor) catch unreachable;
+            if (!gop.found_existing or cost < gop.value_ptr.cost) {
+                gop.value_ptr.* = .{ .cost = cost, .prev = nc.id };
+                nc.open.add(.{ .id = gop.index, .cost = cost }) catch unreachable;
+            }
+        }
+    };
+}
+
+const ExampleContext = struct {
+    target: i64 = 47,
+
+    pub fn expand(_: *@This(), nc: NeighborCollector(i64)) void {
+        nc.add(nc.state + 10, 3);
+        nc.add(nc.state + 1, 1);
+        nc.add(nc.state - 4, 2);
+    }
+
+    pub fn isTerminal(ctx: *@This(), state: i64) bool {
+        return state == ctx.target;
+    }
+};
+
+pub fn search(comptime State: type, comptime Context: type, ctx: *Context, start: State) SearchResult(State) {
+    var open = OpenQueue.init(gpa, {});
+    var states = std.AutoArrayHashMap(State, StateInfo).init(gpa);
+    states.put(start, .{ .cost = 0, .prev = 0 }) catch unreachable;
+    open.add(.{ .id = 0, .cost = 0 }) catch unreachable;
+    const found_state: ?StateCost = while (open.removeOrNull()) |state_id| {
+        const state = states.keys()[state_id.id];
+        const cost = states.values()[state_id.id].cost;
+        if (cost != state_id.cost) continue; // dummy entry
+        if (ctx.isTerminal(state)) break state_id;
+
+        const nc: NeighborCollector(State) = .{
+            .state = state,
+            .id = state_id.id,
+            .cost = cost,
+            .states = &states,
+            .open = &open,
+        };
+        ctx.expand(nc);
+    } else null;
+
+    return .{
+        .states = states,
+        .found_state = found_state,
+    };
+}
+
+test "search" {
+    var ctx = ExampleContext{ .target = 47 };
+    var result = search(i64, ExampleContext, &ctx, 0);
+    try std.testing.expect(result.found_state != null);
+    try std.testing.expectEqual(@as(i64, 47), result.states.keys()[result.found_state.?.id]);
+    try std.testing.expectEqual(@as(usize, 18), result.found_state.?.cost);
+    // print("\n[", .{});
+    // for (result.shortestPathTo(result.found_state.?.id)) |id| {
+    //     if (id != 0) print(",", .{});
+    //     print("({}, {})", .{result.states.keys()[id], result.states.values()[id].cost});
+    // }
+    // print("]\n", .{});
+}
 
 // Useful stdlib functions
 const tokenizeAny = std.mem.tokenizeAny;
